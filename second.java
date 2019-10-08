4929260200e0e1ce4b32ad1397c472b961137b3e
ylongyun <1464238094@qq.com>
2019-10-08 22:43:46 +0800

    java语句熟悉

---
+++ second.java
@@ -0,0 +1,10 @@
+class second{
+	
+	public static void main(String[] args){
+		int m =3 ,n =2 ;
+		n *= ++m;
+		System .out.println("m="+m+"\t"+"n="+n);
+		boolean a=false;
+		System .out.println(a);
+	}
+}
