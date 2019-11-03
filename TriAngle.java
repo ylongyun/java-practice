fb947948b2a3ebc7f14daebbeb1f9cae501f7e6b
ylongyun <1464238094@qq.com>
2019-11-03 18:52:58 +0800

    构造器的使用练习

---
+++ TriAngle.java
@@ -0,0 +1,24 @@
+public class TriAngle {
+    private double base;
+    private double height;
+    public TriAngle(double d,double h){
+        height = h;
+        base = d;
+
+    }
+    public TriAngle(){
+
+    }
+    public double getBase(){
+        return base;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public void setBase(double t){
+        base = t;
+    }
+    public void setHeight(double t){
+        height = t;
+    }
+}
