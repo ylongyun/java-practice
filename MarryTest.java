f825b725b8d28794d262f79b746bb4ecbee4dbb0
ylongyun <1464238094@qq.com>
2019-11-04 21:10:24 +0800

    this的使用练习

---
+++ MarryTest.java
@@ -0,0 +1,10 @@
+public class MarryTest {
+    public static void main(String[] args) {
+        Boy yly = new Boy("ÑîÁúÔÆ",19);
+        Girl zzy = new Girl("ÕÅÔóª",19);
+//        yly.marry(zzy);
+//        yly.shout();
+        zzy.marry(yly);
+        zzy.shout();
+    }
+}
