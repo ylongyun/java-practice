f802d395a50c0bc9e6232621cf17fa1cb36ccd8f
ylongyun <1464238094@qq.com>
2019-11-12 19:06:43 +0800

    综合练习

---
+++ Customer.java
@@ -0,0 +1,27 @@
+public class Customer {
+    private String firstName;
+    private  String lastName;
+    private Account account;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Customer(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+}
