42039740d32f0a09555b4ca6c994b1a9d886a407
ylongyun <1464238094@qq.com>
2019-11-14 15:26:54 +0800

    继承特性的练习

---
+++ KidsTest.java
@@ -0,0 +1,12 @@
+public class KidsTest {
+    public static void main(String[] args) {
+        Kids yuyu = new Kids(6);
+
+        yuyu.printAge();
+
+        yuyu.setSex(0);
+        yuyu.setSalary(0);
+        yuyu.manorWoman();
+        yuyu.employeed();
+    }
+}
