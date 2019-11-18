a04425b09a9140e97e77c80b07888162f6d5d901
ylongyun <1464238094@qq.com>
2019-11-18 20:02:03 +0800

    权限修饰符的测试

---
+++ OrderTest.java
@@ -0,0 +1,10 @@
+public class OrderTest {
+    public static void main(String[] args) {
+        Order order = new Order();
+
+        order.orderDefault = 1;
+        order.orderprotected = 2;
+        order.orderPublic = 3;
+     //   order.orderPrivvate = 4;
+    }
+}
