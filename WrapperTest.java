d41a3e4751cc62974e8daee0bc9668fa87c2cf29
ylongyun <1464238094@qq.com>
2019-11-25 20:12:14 +0800

    基本数据类型转换为包类型

---
+++ WrapperTest.java
@@ -0,0 +1,14 @@
+public class WrapperTest {
+    public static void main(String[] args) {
+        int num1 = 10;
+        Integer in1 = num1;
+        Integer in2 = Integer.valueOf("158");
+        System.out.println(in1.toString(in2));
+        Float f1 = 12.5f;
+        Float f2 = Float.valueOf("12.5");
+        System.out.println(f2.toString());
+        Boolean b1 = Boolean.valueOf("ture123");
+        Boolean b2 = Boolean.valueOf("ss");
+        System.out.println(b1);
+    }
+}
