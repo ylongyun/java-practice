f626ff8608daf9e07dc5ac040eef2ade1c2d00f2
ylongyun <1464238094@qq.com>
2019-11-26 16:08:12 +0800

    使用静态变量完成账号自动生成

---
+++ Account1Test.java
@@ -0,0 +1,8 @@
+public class Account1Test {
+    public static void main(String[] args) {
+        Account1 a1 = new Account1();
+        Account1 a2 = new Account1("123456",2000);
+        System.out.println(a1.toString());
+        System.out.println(a2.toString());
+    }
+}
